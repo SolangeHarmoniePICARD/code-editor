cb7919f28806579dd27de3e2024b0bc9d1e86561
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-15 00:54:54 +0200

    Gestion de la fermeture des tabs.

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openFolder(evt, folderName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,9 +8,34 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(folderName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+// Close London
+document.getElementById("closeLondon").addEventListener("click", closeLondon);
+function closeLondon() {
+  var london = document.getElementById("London") ;
+  london.innerHTML = "";
+  london.style.backgroundColor = "#21252B";
+}
+
+// Close Paris
+document.getElementById("closeParis").addEventListener("click", closeParis);
+function closeParis() {
+  var paris = document.getElementById("Paris") ;
+  paris.innerHTML = "";
+  paris.style.backgroundColor = "#21252B";
+  paris.style.height = "100%";
+}
+
+// Close Tokyo
+document.getElementById("closeTokyo").addEventListener("click", closeTokyo);
+function closeTokyo() {
+  var tokyo = document.getElementById("Tokyo") ;
+  tokyo.innerHTML = "";
+  tokyo.style.backgroundColor = "#21252B";
+}
